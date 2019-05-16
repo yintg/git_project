e33c8431eb6f04eda565bfc16a665576714440d5
yintg <3060196154@qq.com>
2019-05-16 09:01:00 +0800

    初始化

---
+++ student/src/com/nnxy/model/StudentGo.java
@@ -0,0 +1,34 @@
+package com.nnxy.model;
+
+public class StudentGo {
+	private int id;
+	private String name;
+	private int sex;
+	private int age;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSex() {
+		return sex;
+	}
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
